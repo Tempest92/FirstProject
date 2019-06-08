86beab315819523aa8dc2a29d069943ae567b313
tamara.vardanyan998@gmail.com <tamara.vardanyan998@gmail.com>
2019-06-09 00:13:00 +0400

    My code

---
+++ my first project/my first project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("This is my first project");
+            Console.ReadKey();
+        }
+    }
+}
